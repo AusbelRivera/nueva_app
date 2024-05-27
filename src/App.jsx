8d3f92de5e454cb709af39df05686704171701a6
Ausbel Rivera <ausbelrivera@gmail.com>
2024-05-27 09:19:06 -0400

    feat/nuevo_login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Login } from "./Login"
-import "./App.css";
+import "./estilos/App.css";
 
 export function App(){
 
---
+++ src/reutilizable/cabecera.jsx
@@ -0,0 +1,25 @@
+export function Cabecera(){
+
+    return(
+
+        <header className="header">
+            
+            <div className="header_img"></div>
+
+            <button className="header_cerrar">
+
+                Cerrar Sesión
+                
+            </button>
+            
+            <button className="header_Ayuda">
+                
+                Ayuda
+
+            </button>
+
+        </header>
+
+    )
+
+}
