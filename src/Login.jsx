463751e51e71b0cce3dc25f5d6c14a3f70a2b286
AusbelRivera <ausbelrivera@gmail.com>
2024-05-27 10:37:37 -0400

    feat/nuevo_login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,42 +1,36 @@
-import './estilos/Login.css'
+import './Login.css'
 
 export function Login(){
 
-    const RegistrarUsuario = () => {
-        
-        window.open('https://www.facebook.com', '_blank', 'width=600,height=400');
-
-      };
+    
 
     return(
 
-        <>
-            <form action='' className="contenedor">
+        <form action='' className="contenedor">
 
-                <div className='contenedor_caja1'>
-                    
-                    <h2 className='contenedor_caja1_h2'>Ingresa tu usuario:</h2>
+            <div className='contenedor_caja1'>
+                
+                <h2 className='contenedor_caja1_h2'>Ingresa tu usuario:</h2>
 
-                    <input type="text" placeholder='nombre de usuario' className='contenedor_caja1_input' />
+                <input type="text" placeholder='nombre de usuario' className='contenedor_caja1_input' />
 
-                </div>
+            </div>
 
-                <div className='contenedor_caja2'>
-                    
-                    <h2 className='contenedor_caja2_h2'>Ingresa tu Contraseña:</h2>
+            <div className='contenedor_caja2'>
+                
+                <h2 className='contenedor_caja2_h2'>Ingresa tu Contraseña:</h2>
 
-                    <input type="password" placeholder='Contraseña' className='contenedor_caja2_input' />
+                <input type="password" placeholder='Contraseña' className='contenedor_caja2_input' />
 
-                </div>
+            </div>
 
-                <div className='contenedor_botones'> 
-                    <button className='contenedor_boton1' >Ingresar</button>
-                </div>
+            <div className='contenedor_botones'> 
+                <button className='contenedor_boton1'>Ingresar</button>
+                <button className='contenedor_boton2'>Registrar</button>
+            </div>
 
-            </form>
 
-            <button className='contenedor_boton2' onClick={RegistrarUsuario}>Registrar</button>
-         </>
+        </form>
             
         
 
